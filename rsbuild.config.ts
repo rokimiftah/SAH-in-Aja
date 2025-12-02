99ef978c34641914fb27e4c51cc330adf3c75f42
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2025-12-02 10:45:36 +0700

    initial commit

---
+++ rsbuild.config.ts
@@ -0,0 +1,32 @@
+import path from "node:path";
+
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+
+// Docs: https://rsbuild.rs/config/
+export default defineConfig({
+  output: {
+    assetPrefix: "/",
+  },
+
+  performance: {
+    removeConsole: true,
+  },
+
+  plugins: [pluginReact()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@app": path.resolve(__dirname, "src/app"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+    },
+  },
+
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+});
